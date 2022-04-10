b63a1446a7c3736ad450dbda077df26301766888
J. Salas <salasvideo@yahoo.com>
2022-04-10 11:14:35 -0500

    Added project source

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+#viewed file 4/8
+#Udacity project
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
